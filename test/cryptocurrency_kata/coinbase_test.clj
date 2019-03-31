7fb280455b106d5a0daf59466d7059910dca9933
Esko Luontola <esko@luontola.fi>
2019-04-01 00:01:39 +0300

    Extract test-data namespace

--- test/cryptocurrency_kata/coinbase_test.clj
+++ test/cryptocurrency_kata/coinbase_test.clj
@@ -1,13 +1,8 @@
 (ns cryptocurrency-kata.coinbase-test
-  (:require [clojure.edn :as edn]
-            [clojure.java.io :as io]
-            [clojure.test :refer :all]
+  (:require [clojure.test :refer :all]
             [cryptocurrency-kata.coinbase :as coinbase]
-            [cryptocurrency-kata.core :as core]
-            [medley.core :refer [map-vals]]
-            [zprint.core :as zp])
-  (:import (java.io File)
-           (java.time Instant)
+            [medley.core :refer [map-vals]])
+  (:import (java.time Instant)
            (java.util UUID)))
 
 (set! *warn-on-reflection* true)
@@ -30,44 +25,3 @@
           (str "type,time,amount,balance,amount/balance unit,transfer id,trade id,order id\n"
                "deposit,2018-02-04T14:48:06.142Z,0.0099876500000000,0.0099876500000000,BTC,8afb99ca-b8c3-4405-b4de-30bf2e7a0c86,,\n"
                "match,2018-02-04T21:09:11.089Z,-0.0099876500000000,0.0000000000000000,BTC,,11309314,53d6f6cc-db5e-40d6-aab6-a9e5cc2f0c46\n")))))
-
-(defn- strip-types [rows]
-  (map (fn [row]
-         (->> (assoc row :time (.toString ^Instant (:time row)))
-              (map-vals (fn [val]
-                          (if (instance? UUID val)
-                            (.toString ^UUID val)
-                            val)))))
-       rows))
-
-(def ^:private zprint-opts
-  {:map {:comma? false
-         :force-nl? true
-         :key-order [:type :time :currency :amount :balance
-                     :transfer-id :trade-id :order-id
-                     :source :target :fee]}})
-
-(defn- format-for-edn [rows]
-  (-> ^String (zp/zprint-str (vec rows) zprint-opts)
-      (.replace " 0E-16M\n" " 0.0000000000000000M\n")))
-
-(defn- convert-files [^File dir]
-  (doseq [^String filename (.list dir)]
-    (when (.endsWith filename ".csv")
-      (let [input (io/file dir filename)
-            output (io/file dir (.replaceFirst filename ".csv$" ".edn"))]
-        (assert (not= input output))
-        (println (str input) "->" (str output))
-        (->> (slurp input)
-             (coinbase/parse-account-report-csv)
-             (strip-types)
-             (format-for-edn)
-             (spit output))))))
-
-(comment
-  (convert-files (io/file "data"))
-  (->> (slurp "data/all.edn")
-       (edn/read-string)
-       (core/group-by-trade)
-       (format-for-edn)
-       (spit "data/all-grouped.edn")))
---
+++ test/cryptocurrency_kata/test_data.clj
@@ -0,0 +1,55 @@
+(ns cryptocurrency-kata.test-data
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.test :refer :all]
+            [cryptocurrency-kata.coinbase :as coinbase]
+            [cryptocurrency-kata.core :as core]
+            [medley.core :refer [map-vals]]
+            [zprint.core :as zp])
+  (:import (java.io File)
+           (java.time Instant)
+           (java.util UUID)))
+
+(set! *warn-on-reflection* true)
+
+(defn- strip-types [rows]
+  (map (fn [row]
+         (->> (assoc row :time (.toString ^Instant (:time row)))
+              (map-vals (fn [val]
+                          (if (instance? UUID val)
+                            (.toString ^UUID val)
+                            val)))))
+       rows))
+
+(def ^:private zprint-opts
+  {:map {:comma? false
+         :force-nl? true
+         :key-order [:type :time :currency :amount :balance
+                     :transfer-id :trade-id :order-id
+                     :source :target :fee]}})
+
+(defn- format-for-edn [rows]
+  (-> ^String (zp/zprint-str (vec rows) zprint-opts)
+      (.replace " 0E-16M\n" " 0.0000000000000000M\n")))
+
+(defn- convert-files [^File dir]
+  (doseq [^String filename (.list dir)]
+    (when (.endsWith filename ".csv")
+      (let [input (io/file dir filename)
+            output (io/file dir (.replaceFirst filename ".csv$" ".edn"))]
+        (assert (not= input output))
+        (println (str input) "->" (str output))
+        (->> (slurp input)
+             (coinbase/parse-account-report-csv)
+             (strip-types)
+             (format-for-edn)
+             (spit output))))))
+
+(comment
+  (convert-files (io/file "data"))
+
+  (->> (slurp "data/all.edn")
+       (edn/read-string)
+       (core/group-by-trade)
+       (format-for-edn)
+       (spit "data/all-grouped.edn")))
