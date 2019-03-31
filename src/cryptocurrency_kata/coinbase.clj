ae65c5178c4459254b3edf4b2a138fbb370944c4
Esko Luontola <esko@luontola.fi>
2019-03-31 20:23:55 +0300

    Parse account report CSV files from Coinbase

---
+++ src/cryptocurrency_kata/coinbase.clj
@@ -0,0 +1,52 @@
+(ns cryptocurrency-kata.coinbase
+  (:require [clojure.data.csv :as csv]
+            [clojure.edn :as edn]
+            [clojure.string :as str])
+  (:import (java.time Instant)
+           (java.util UUID List)))
+
+(set! *warn-on-reflection* true)
+
+(defn- assoc-column-index [column header-row]
+  (let [index (.indexOf ^List header-row (:header column))]
+    (assert (not (neg? index)) column)
+    (assoc column :column-index index)))
+
+(defn- parse-row [row columns]
+  (->> row
+       (map-indexed (fn [index value]
+                      (let [column (nth columns index)
+                            parser (or (:parser column)
+                                       identity)]
+                        (when (not (str/blank? value))
+                          [(:key column) (parser value)]))))
+       (into {})))
+
+(defn parse-account-report-csv [content]
+  (let [[header-row & rows] (csv/read-csv content)
+        columns (->> [{:header "type"
+                       :key :type
+                       :parser keyword}
+                      {:header "time"
+                       :key :time
+                       :parser #(Instant/parse %)}
+                      {:header "amount"
+                       :key :amount
+                       :parser #(BigDecimal. ^String %)}
+                      {:header "balance"
+                       :key :balance
+                       :parser #(BigDecimal. ^String %)}
+                      {:header "amount/balance unit"
+                       :key :currency
+                       :parser keyword}
+                      {:header "transfer id"
+                       :key :transfer-id
+                       :parser #(UUID/fromString %)}
+                      {:header "trade id"
+                       :key :trade-id
+                       :parser edn/read-string}
+                      {:header "order id"
+                       :key :order-id
+                       :parser #(UUID/fromString %)}]
+                     (map #(assoc-column-index % header-row)))]
+    (map #(parse-row % columns) rows)))
---
+++ test/cryptocurrency_kata/coinbase_test.clj
@@ -0,0 +1,24 @@
+(ns cryptocurrency-kata.coinbase-test
+  (:require [clojure.test :refer :all]
+            [cryptocurrency-kata.coinbase :as coinbase])
+  (:import (java.time Instant)
+           (java.util UUID)))
+
+(deftest test-parse-account-report-csv
+  (is (= [{:type :deposit
+           :time (Instant/parse "2018-02-04T14:48:06.142Z")
+           :amount 0.0099876500000000M
+           :balance 0.0099876500000000M
+           :currency :BTC
+           :transfer-id (UUID/fromString "8afb99ca-b8c3-4405-b4de-30bf2e7a0c86")}
+          {:type :match
+           :time (Instant/parse "2018-02-04T21:09:11.089Z")
+           :amount -0.0099876500000000M
+           :balance 0.0000000000000000M
+           :currency :BTC
+           :trade-id 11309314
+           :order-id (UUID/fromString "53d6f6cc-db5e-40d6-aab6-a9e5cc2f0c46")}]
+         (coinbase/parse-account-report-csv
+          (str "type,time,amount,balance,amount/balance unit,transfer id,trade id,order id\n"
+               "deposit,2018-02-04T14:48:06.142Z,0.0099876500000000,0.0099876500000000,BTC,8afb99ca-b8c3-4405-b4de-30bf2e7a0c86,,\n"
+               "match,2018-02-04T21:09:11.089Z,-0.0099876500000000,0.0000000000000000,BTC,,11309314,53d6f6cc-db5e-40d6-aab6-a9e5cc2f0c46\n")))))
