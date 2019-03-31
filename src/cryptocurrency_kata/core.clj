c3e15bf3d28059a8b2f55571dce05e415c15499a
Esko Luontola <esko@luontola.fi>
2019-03-31 22:12:30 +0300

    group-by-trade

--- src/cryptocurrency_kata/core.clj
+++ src/cryptocurrency_kata/core.clj
@@ -1,1 +1,10 @@
 (ns cryptocurrency-kata.core)
+
+(defn group-by-trade [txs]
+  (->> (group-by :trade-id txs)
+       (map (fn [[_trade-id txs]]
+              (assert (= 2 (count txs)))
+              (let [[from to] (sort-by :amount txs)]
+                {:to to
+                 :from from})))
+       (sort-by (comp :time :from))))
