70d3dd16cb252d6b7c8fed79e783f4d120af4428
Esko Luontola <esko@luontola.fi>
2019-03-31 22:35:19 +0300

    group-by-trade: merge shared keys

--- src/cryptocurrency_kata/core.clj
+++ src/cryptocurrency_kata/core.clj
@@ -1,12 +1,23 @@
 (ns cryptocurrency-kata.core)
 
+(set! *warn-on-reflection* true)
+
+(def ^:private shared-match-keys [:type :time :trade-id :order-id])
+
 (defn group-by-trade [txs]
   (->> txs
        (group-by #(or (:trade-id %) identity))
        (map (fn [[_ txs]]
               (case (count txs)
                 1 (first txs)
-                2 (let [[from to] (sort-by :amount txs)]
-                    {:to to
-                     :from from}))))
-       (sort-by (comp :time :from))))
+                2 (let [[from to] (sort-by :amount txs)
+                        _ (assert (= :match (:type from)) from)
+                        shared-from (select-keys from shared-match-keys)
+                        shared-to (select-keys from shared-match-keys)
+                        from (apply dissoc from shared-match-keys)
+                        to (apply dissoc to shared-match-keys)]
+                    (assert (= shared-from shared-to))
+                    (assoc shared-from
+                      :from from
+                      :to to)))))
+       (sort-by :time)))
--- test/cryptocurrency_kata/core_test.clj
+++ test/cryptocurrency_kata/core_test.clj
@@ -2,36 +2,30 @@
   (:require [clojure.test :refer :all]
             [cryptocurrency-kata.core :as core]))
 
+(set! *warn-on-reflection* true)
+
 (deftest test-group-by-trade
   (testing "matches the source and target accounts"
-    (is (= [{:from {:type :match
-                    :time "2018-02-04T21:45:51.354Z"
-                    :amount -64.3206146174000000M
+    (is (= [{:type :match
+             :time "2018-02-04T21:45:51.354Z"
+             :trade-id 11311696
+             :order-id "37f1a4bd-4f87-43a5-9b80-641598d60e54"
+             :from {:amount -64.3206146174000000M
                     :balance 0.0000511356000000M
-                    :currency :EUR
-                    :trade-id 11311696
-                    :order-id "37f1a4bd-4f87-43a5-9b80-641598d60e54"}
-             :to {:type :match
-                  :time "2018-02-04T21:45:51.354Z"
-                  :amount 0.0098621300000000M
+                    :currency :EUR}
+             :to {:amount 0.0098621300000000M
                   :balance 0.0098621300000000M
-                  :currency :BTC
-                  :trade-id 11311696
-                  :order-id "37f1a4bd-4f87-43a5-9b80-641598d60e54"}}
-            {:from {:type :match
-                    :time "2018-02-04T21:48:48.944Z"
-                    :amount -0.0098621300000000M
+                  :currency :BTC}}
+            {:type :match
+             :time "2018-02-04T21:48:48.944Z"
+             :trade-id 11311817
+             :order-id "780ce36b-bc09-4534-869b-330394800769"
+             :from {:amount -0.0098621300000000M
                     :balance 0.0000000000000000M
-                    :currency :BTC
-                    :trade-id 11311817
-                    :order-id "780ce36b-bc09-4534-869b-330394800769"}
-             :to {:type :match
-                  :time "2018-02-04T21:48:48.944Z"
-                  :amount 64.3774204862000000M
+                    :currency :BTC}
+             :to {:amount 64.3774204862000000M
                   :balance 64.3774716218000000M
-                  :currency :EUR
-                  :trade-id 11311817
-                  :order-id "780ce36b-bc09-4534-869b-330394800769"}}]
+                  :currency :EUR}}]
            (core/group-by-trade
             [{:type :match
               :time "2018-02-04T21:45:51.354Z"
