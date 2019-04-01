0bd74d203cba59122102a26e1bb4ca7e03ecbaf8
Esko Luontola <esko@luontola.fi>
2019-04-01 23:43:28 +0300

    Refactor

--- src/cryptocurrency_kata/money.clj
+++ src/cryptocurrency_kata/money.clj
@@ -10,25 +10,24 @@
   (assert (:amount coin) {:coin coin})
   (assert (:currency coin) {:coin coin})
   (assert (pos? split-amount) {:split-amount split-amount})
-  (if (>= split-amount (:amount coin))
+  (cond
+    (>= split-amount (:amount coin))
     [coin]
-    ;; TODO: refactor the case of no original value
-    (let [original-value (:amount (or (:original-value coin)
-                                      coin))
-          _ (assert original-value {:coin coin})
-          remaining-amount (- (:amount coin) split-amount)
-          remaining-value (* original-value (/ remaining-amount (:amount coin)))
-          remaining (-> coin
-                        (assoc :amount remaining-amount)
-                        (cond->
-                          (:original-value coin) (assoc-in [:original-value :amount] remaining-value)))
-          taken-amount (- (:amount coin) remaining-amount)
-          taken-value (- original-value remaining-value)
-          taken (-> coin
-                    (assoc :amount taken-amount)
-                    (cond->
-                      (:original-value coin) (assoc-in [:original-value :amount] taken-value)))]
-      [taken remaining])))
+
+    (:original-value coin)
+    (let [[taken remain] (split-coin (select-keys coin [:amount :currency]) split-amount)
+          orig-taken-amount (* (:amount (:original-value coin))
+                               (/ (:amount taken)
+                                  (:amount coin)))
+          [orig-taken orig-remain] (split-coin (:original-value coin) orig-taken-amount)]
+      [(assoc taken :original-value orig-taken)
+       (assoc remain :original-value orig-remain)])
+
+    :else
+    (let [remain-amount (- (:amount coin) split-amount)
+          taken-amount (- (:amount coin) remain-amount)]
+      [(assoc coin :amount taken-amount)
+       (assoc coin :amount remain-amount)])))
 
 (defn take-coins
   ([coins wanted]
