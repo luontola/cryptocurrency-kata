598621878a48c166f320a7c36a826818f80088cb
Esko Luontola <esko@luontola.fi>
2019-04-01 20:00:14 +0300

    Refactor: extract split-coin

--- src/cryptocurrency_kata/money.clj
+++ src/cryptocurrency_kata/money.clj
@@ -6,6 +6,24 @@
   {:amount (+ (:amount a) (:amount b))
    :currency (:currency a)})
 
+(defn- split-coin [coin split-amount]
+  (assert (pos? split-amount)
+          split-amount)
+  (if (>= split-amount (:amount coin))
+    [coin]
+    (let [coin-original-value (:amount (:original-value coin))
+          coin-remaining-amount (- (:amount coin) split-amount)
+          coin-taken-amount (- (:amount coin) coin-remaining-amount)
+          coin-remaining-value (* coin-original-value (/ coin-remaining-amount (:amount coin)))
+          coin-taken-value (- coin-original-value coin-remaining-value)
+          taken (-> coin
+                    (assoc :amount coin-taken-amount)
+                    (assoc-in [:original-value :amount] coin-taken-value))
+          remaining (-> coin
+                        (assoc :amount coin-remaining-amount)
+                        (assoc-in [:original-value :amount] coin-remaining-value))]
+      [taken remaining])))
+
 (defn take-coins
   ([coins wanted]
    (take-coins coins wanted []))
@@ -16,33 +34,10 @@
      (do
        (assert (neg? (:amount wanted))
                {:wanted wanted})
-       (let [coin (first coins)
-             coin-original-value (:amount (:original-value coin))
-             coin-remaining-amount (+ (:amount coin) (:amount wanted)) ;; TODO cap to zero
-             coin-taken-amount (- (:amount coin) coin-remaining-amount)
-             coin-remaining-value (* coin-original-value (/ coin-remaining-amount (:amount coin)))
-             coin-taken-value (- coin-original-value coin-remaining-value)
-             taken (-> coin
-                       (assoc :amount coin-taken-amount)
-                       (assoc-in [:original-value :amount] coin-taken-value))
-             remaining (-> coin
-                           (assoc :amount coin-remaining-amount)
-                           (assoc-in [:original-value :amount] coin-remaining-value))]
-
-         ;;(println "----------")
-         ;;(prn 'coin coin)
-         ;;(prn 'wanted wanted)
-         ;;(prn 'taken taken)
-         ;;(prn 'remaining remaining)
-         ;;(prn 'coin-taken-amount coin-taken-amount)
-         ;;(prn 'coin-remaining-amount coin-remaining-amount)
-         ;;(prn 'coin-taken-value coin-taken-value)
-         ;;(prn 'coin-remaining-value coin-remaining-value)
-
-         (if (pos? coin-remaining-amount)
-           (take-coins (concat [remaining] (rest coins))
-                       (assoc wanted :amount 0M)
-                       [taken])
-           (take-coins (rest coins)
-                       (assoc wanted :amount 0M)
-                       [coin])))))))
+       (let [[taken-coin & remaining-coins] (concat (split-coin (first coins)
+                                                                (- (:amount wanted)))
+                                                    (rest coins))
+             still-wanted (sum wanted taken-coin)]
+         (take-coins remaining-coins
+                     still-wanted
+                     (conj taken-coins taken-coin)))))))
